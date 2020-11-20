45637cdba6da376a1d04da8215fa7001d870290b
Szabolcs Szabolcsi-Toth <nec@shell8.net>
2020-11-20 15:11:04 +0100

    Update no-long-pipeline.ts

--- src/rules/no-long-pipeline.ts
+++ src/rules/no-long-pipeline.ts
@@ -1,6 +1,6 @@
 export default (docs, tekton, report) => {
   for (const pipeline of Object.values<any>(tekton.pipelines)) {
-    if (pipeline.spec.tasks.length > 33) {
+    if (pipeline.spec.tasks.length > 42) {
       report(`Pipeline '${pipeline.metadata.name}' is too long.`, pipeline.spec);
     }
   }
